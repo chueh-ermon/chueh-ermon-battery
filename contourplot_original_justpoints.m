7fdff4d3a982179c8d404d5815bbd932e3de28ca
chueh-battery <chueh-ermon@users.noreply.github.com>
2017-06-20 13:31:48 -0700

    save contourplot progress

--- contourplot_original_justpoints.m
+++ contourplot_original_justpoints.m
@@ -4,21 +4,24 @@
 
 close all
 
-Q1=5:5:80;
+colormap jet
+
+Q1=0:5:80;
 CC1=3:0.5:10;
 CC2=[3 3.6];
 time = ones(length(CC2),length(CC1),length(Q1));
 figure, set(gcf, 'units','normalized','outerposition',[0 0 1 1])
 for i=1:length(CC2)
     subplot(1,length(CC2),i), hold on, box on
-    xlabel('C1'),ylabel('SOC1 (%)')
-    title(['C2 = ' num2str(CC2(i))])
-    %axis([min(CC1) max(CC1) min(Q1) max(Q1)])
+    xlabel('C1', 'FontSize',18),ylabel('SOC1 (%)', 'FontSize',18)
+    title(['C2 = ' num2str(CC2(i))], 'FontSize',18)
+    axis([min(CC1) max(CC1) 0 max(Q1)])
     
     [X,Y] = meshgrid(CC1,Q1);
     time = (60.*Y./X./100)+(60.*(80-Y)./CC2(i)./100);
-    v = [13, 12, 11, 10, 9, 8];
+    v = [14,13, 12, 11, 10, 9];
     contour(X,Y,time,v,'LineWidth',2,'ShowText','on')
+    set(gca, 'FontSize',18)
 end
 
 % Manually determined degradation values in Excel. The list of policies and
@@ -47,8 +50,6 @@ list = [3.6	80	3.601	3.1279E-05
         8	25	3.6	2.7165E-04
         8	35	3.6	3.1434E-04];
 
-colormap jet;
-
 scalefactor = 1e6; % Factor to scale 
 
 maxvalue = max(list(:,4))*scalefactor;
@@ -65,10 +66,10 @@ for i = 1:length(list(:,1))
     else
         subplot(1, length(CC2), 1)
         scatter(list(i,1),list(i,2),'square','filled','CData',[0 0 0],'SizeData',200,'LineWidth',5)
-        caxis([0 maxvalue])
+        %caxis([0 maxvalue])
         subplot(1, length(CC2), 2)
         scatter(list(i,1),list(i,2),'square','filled','CData',[0 0 0],'SizeData',200,'LineWidth',5)
-        caxis([0 maxvalue])
+        %caxis([0 maxvalue])
     end
 end
 

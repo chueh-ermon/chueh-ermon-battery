42e4f9e77e332a1a139f3ca4ed5f94491ebe4c0d
Zi <ziyang@Zis-MacBook-Pro.local>
2017-06-27 16:44:06 -0700

    working on full_analysis and full_cell_analysis

--- full_analysis.m
+++ full_analysis.m
@@ -13,15 +13,14 @@ cd 'C://Data'
 % struct called cells - what would it hold?
 % policy, barcode, dQdV curves/cycle, relative capacity/cycle, voltage
 % range (is this the same for all policies?), temperature
-%            -
+%            ----
 %           | policy (string)
 %           | barcode (string)
 % cells(1) -  dQdV curves/cycle (cell array of cell arrays)
 %           | relative capacity/cycle (cell array)
 %           | voltage range (?)
 %           | temperature
-%            -
-
+%            ----
 
 %% For each file, collect data needed
 % If just one file
@@ -29,9 +28,13 @@ if isa(filename,'char')
     
 % If multiple files
 elseif isa(filename,'cell') % If multiple files
-    numcsvs = length(filename); % Number of CSVs selected
-    for j=1:numcsvs  
+    num_csvs = length(filename); % Number of CSVs selected
+    for j=1:num_csvs  
+        raw_data = csvread(strcat(pathname,filename{j}),1,1);
+        [cell] = full_cell_analysis(raw_data);
+        cells(j) = cell;
         % Run file through full_cell_analysis
+        % find cell barcode in metadata file - add to cell
         % concatanate pulled data to a cell in cells
     end
 end
--- full_cell_analysis.m
+++ full_cell_analysis.m
@@ -3,10 +3,42 @@
 %
 % In-depth analysis of dQdV, temperature, relative capacity, and voltage
 % for each cycle in cell
+%            ----
+%           | policy (string)
+%           | barcode (string)
+% cells(1) -  dQdV curves/cycle (cell array of cell arrays)
+%           | relative capacity/cycle (cell array)
+%           | voltage range (?)
+%           | temperature
+%            ----
+
+function [cell] = full_cell_analysis(raw_data)
 
 %% Initialize struct/object/cellarray
+% may not be able to retrieve this here? cell.policy = 'string';
+% may not be able to retrieve this here? cell.barcode = 'string';
+cell.dQdV_curves = {{},{}};
+cell.capacity = {};
+cell.voltage = {};
+cell.temperature = {};
+
+% total_time = raw_data(:,1);
+% step_time = raw_data(:,3);
+% step_index = round(raw_data(:,4));
+ cycle_index = raw_data(:,5);
+% voltageV = raw_data(:,7);
+% current = raw_data(:,6);
+% charge_capacityAh = raw_data(:,8);
+% discharge_capacityAh = raw_data(:,9);
 
 %% For each cycle, collect information
+for j = max(cycle_index) % - 1 (do we include last cycle?)
+    i1 = find(cycle_index == j);
+    i1a = i1(1); i1b = i1(end);
+end
+
+
+
 
 %% Save Matlab Workspace as a .mat file
 

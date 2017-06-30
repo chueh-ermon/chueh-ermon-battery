421201700b9df5bf459439d47147cc82ce58325d
chueh-battery <chueh-ermon@users.noreply.github.com>
2017-06-30 10:53:48 -0700

    make some changes to batteries struct code

--- Batch_Analysis.m
+++ Batch_Analysis.m
@@ -59,7 +59,7 @@ for i = 1:numel(filenames)
     end
 end
 filenames = filenames(1:numel(filenames)-deletedcount);
-foldername = battery(1,numel(filenames)); % TODO: can we delete this
+foldername = cell(1,numel(filenames)); % TODO: can we delete this
 % In case there were no files found.
 if numel(filenames) == 0
     disp('No files match query')
@@ -125,6 +125,11 @@ for j= 1:numel(CA_array)
             [Charge_time,dDQdV,End_of_life, cycle, ~, DQ, cell_ID1, ...
                 test_time, battery] = Cell_Analysis(ResultData, j, ...
                 CA_array{j}, barcodes{i}, charging_algorithm);
+            batteries = struct('policy', ' ', 'barcode', ' ', 'cycles', ...
+                struct('discharge_dQdVvsV', struct('V', [], 'dQdV', [])), ...
+                'summary', struct('cycle', [], 'QDischarge', [], 'QCharge', ...
+                [], 'IR', [], 'tmax', [], 'tavg', [], 'tmin', [], ...
+                'chargetime', [], 'relative_capacity', []));
             batteries(i) = battery;
             num_batt=num_batt+1;
             

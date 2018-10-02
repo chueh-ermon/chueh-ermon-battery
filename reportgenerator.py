ef351ac835b7e3e8546ae45a12266049f8e03b82
petermattia <pattia@stanford.edu>
2018-10-02 10:51:33 -0700

    reportgenerator

--- reportgenerator.py
+++ reportgenerator.py
@@ -9,16 +9,17 @@ Created on Fri Jul  7 12:10:30 2017
 # Imports
 import os # file i/o operations
 import glob # file i/o operations
-from datetime import time# finding today's date
+from datetime import date# finding today's date
 from pptx import Presentation # creating the PPT
-from pptx.util import Inches
-import comtypes.client # for opening PowerPoint from python
+from pptx.util import Inches, Pt
+#import comtypes.client # for opening PowerPoint from python
 
+"""
 def PPTtoPDF(inputFileName, outputFileName, formatType = 32):
-    """
+    
     Converts a PPT file to a PDF by opening PowerPoint, opening the file, and 
     then saving as a PowerPoint. Requires Windows. 
-    """
+    
     powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
     powerpoint.Visible = 1
 
@@ -28,14 +29,26 @@ def PPTtoPDF(inputFileName, outputFileName, formatType = 32):
     deck.SaveAs(outputFileName, formatType) # formatType = 32 for ppt to pdf
     deck.Close()
     powerpoint.Quit()
+"""
+
+def addTitleSlide(title,subtitle):
+    slide = prs.slides.add_slide(title_only_slide_layout)
+    #left = Inches(2)
+    #top = width = height = Inches(1)
+    #txBox = slide.shapes.add_textbox(left, top, width, height)
+    #tf = txBox.text_frame
+    shapes = slide.shapes
+    shapes.title.text = title
+    #p = tf.add_paragraph()
+    #p.text = title
+    #p.font.size = Pt(40)
 
 def addImageSlide(imageFileName):
     """
     Adds a full-screen image to a blank full-screen slide.
     """
-    blank_slide_layout = prs.slide_layouts[6]
     slide = prs.slides.add_slide(blank_slide_layout)
-    pic = slide.shapes.add_picture(img_path, 0, 0, height=prs.slide_height, width=prs.slide_width)
+    slide.shapes.add_picture(imageFileName, 0, 0, height=prs.slide_height, width=prs.slide_width)
 
 # Get today's date, formatted to MATLAB's default (e.g. 2017-Jul-09)
 today = date.today().strftime('%d-%b-%Y')
@@ -46,26 +59,27 @@ reportFile = today + '_report.pptx'
 # Initialize presentation
 prs = Presentation()
 prs.slide_height = 5143500 # Widescreen aspect ratio
-title_slide_layout = prs.slide_layouts[0] # add title slide
-slide = prs.slides.add_slide(title_slide_layout)
-title = slide.shapes.title
-subtitle = slide.placeholders[1]
+title_slide_layout = prs.slide_layouts[1]
+blank_slide_layout = prs.slide_layouts[6] # blank slide
 
 # Create title slide
-title.text = "Current Cycling Progress"
-subtitle.text = today
+addTitleSlide('Current Cycling Progress', today)
 
 # CD to directory with most recent images
-os.chdir('C:\\Users\\Arbin\\Box Sync\\Batch images\\' + today + '\\')
+# os.chdir('C:\\Users\\Arbin\\Box Sync\\Batch images\\' + today + '\\')
 
-# Add summary slide
-img_path = 'contour.png'
-addImageSlide(img_path)
+# Add .png files in this directory. Start with summary figures
+all_images = glob.glob('*.png')
+for file in all_images:
+    if "summary" in file:
+        addImageSlide(file)
+        
+# Cell "spec sheets"
+for file in all_images:
+    if "summary" not in file:
+        addImageSlide(file)
 
-# Add each cell "spec sheet" .png files in this directory
-for file in glob.glob('*.png'):
-    addImageSlide(file)
-
+"""
 # Directory for saving reports
 saveDir = 'C:\\Users\\Arbin\\Box Sync\\Reports'
 os.chdir(saveDir)
@@ -77,4 +91,5 @@ reportFileFullPDF = saveDir + '\\' + reportFile.replace('pptx','pdf')
 # Save powerpoint
 prs.save(reportFileFull)
 # Convert to PDF
-PPTtoPDF(reportFileFull,reportFileFullPDF)
+#PPTtoPDF(reportFileFull,reportFileFullPDF)
+"""
